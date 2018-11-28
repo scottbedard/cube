38d15f6f7f0a6c9a4e3e659b1753b44317c67bfd
Scott Bedard <scottbedard90@gmail.com>
2018-11-27 22:05:01 -0700

    add cli command to turn a cube closes #9

--- cli.js
+++ cli.js
@@ -11,11 +11,28 @@ program.version(pkg.version, '-v, --version');
 program
     .command('scramble [size] [length]')
     .description('generate a scramble for a cube of a given size')
-    .action(function(size, length) {
-        size = Number(size) || 3;
-        length = Number(length) || 0;
-        
+    .action(function(size, length = 0) {
+        size = Number(size);
+        length = Number(length);
+
         console.log(new Cube(size).generateScrambleString(length));
+    });
+
+//
+// turn
+//
+program
+    .command('turn [size] [turns]')
+    .description('apply a series of turns to a cube of a given size')
+    .action(function(size, turns) {
+        size = Number(size);
+        turns = String(turns);
+
+        var cube = new Cube(size);
+
+        cube.turn(turns);
+
+        console.log(JSON.stringify(cube.state));
     });
 
 program.parse(process.argv);
